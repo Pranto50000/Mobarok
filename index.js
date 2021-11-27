c75ae7679b958dbabf80884f5a2a7a1d7e09a6b7
Pranto Kumar Sarker <90765689+Pranto50000@users.noreply.github.com>
2021-11-28 00:24:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '09, November, 2002',
+  birthdate: '009, November, 2002',
   name: 'SAHANI PARVIN'
 };
 
